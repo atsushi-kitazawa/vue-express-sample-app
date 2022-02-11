1dbe24bd211685d2dc175f9f8237e685f3853f7f
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2022-02-11 22:40:57 +0900

    create user controller.

---
+++ backend/controllers/user_controller.js
@@ -0,0 +1,68 @@
+const db = require('../models')
+const Account = db.account
+
+exports.findAll = (req, res) => {
+    Account.findAll().then(result => {
+        res.status(200).json(result);
+    }).catch(err => {
+        res.status(500).send('Error: ' + err);
+    });
+}
+
+exports.findByPk = (req, res) => {
+    Account.findByPk(req.params.userId).then(result => {
+        if (result != null) {
+            res.status(200).json(result);
+        } else {
+            res.status(404).json({ msg: 'user not found' });
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).send('Error: ' + err);
+    })
+}
+
+exports.create = (req, res) => {
+    Account.create({
+        id: req.body.id,
+        name: req.body.name
+    }).then(result => {
+        res.status(200).json({
+            id: req.body.id,
+            name: req.body.name
+        });
+    }).catch(err => {
+        res.status(500).send('Error: ' + err);
+    })
+}
+
+exports.update = (req, res) => {
+    Account.update({
+        name: req.body.name
+    }, {
+        where: {
+            id: req.params.userId
+        }
+    }).then(result => {
+        res.status(200).json({
+            id: req.params.userId,
+            name: req.body.name
+        }).catch(err => {
+            res.status(500).send('Error: ' + err);
+        })
+    })
+}
+
+exports.destroy = (req, res) => {
+    Account.destroy({
+        where: {
+            id: req.params.userId
+        }
+    }).then(result => {
+        res.status(200).json({
+            id: req.params.userId
+        }).catch(err => {
+            res.status(500).send('Error: ' + err);
+        })
+    })
+}
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,77 +1,15 @@
 const e = require('express');
 var express = require('express');
+const db = require('../models');
+const controller = require('../controllers/user_controller.js');
 var router = express.Router();
-const db = require('../models')
-db.sequelize.sync();
 const Account = db.account
+db.sequelize.sync();
 
-var fn1 = function (req, res, next) {
-  Account.findAll().then(result => {
-    res.status(200).json(result);
-  }).catch(err => {
-    res.status(500).send('Error: ' + err);
-  });
-  // next() 配列で指定した次の関数を実行してくれる
-}
-
-/* GET users listing. */
-router.get('/', [fn1]);
-
-router.get('/:userId', function (req, res, next) {
-  Account.findByPk(req.params.userId).then(result => {
-    if (result != null) {
-      res.status(200).json(result);
-    } else {
-      res.status(404).json({ msg: 'user not found' });
-    }
-  }).catch(err => {
-    res.status(500).send('Error: ' + err);
-  })
-});
-
-router.post('/', function (req, res, next) {
-  Account.create({
-    id: req.body.id,
-    name: req.body.name
-  }).then(result => {
-    res.status(200).json({
-      id: req.body.id,
-      name: req.body.name
-    });
-  }).catch(err => {
-    res.status(500).send('Error: ' + err);
-  })
-})
-
-router.put('/:userId', function (req, res, next) {
-  Account.update({
-    name: req.body.name
-  }, {
-    where: {
-      id: req.params.userId
-    }
-  }).then(result => {
-    res.status(200).json({
-      id: req.params.userId,
-      name: req.body.name
-    }).catch(err => {
-      res.status(500).send('Error: ' + err);
-    })
-  })
-})
-
-router.delete('/:userId', function (req, res, next) {
-  Account.destroy({
-    where: {
-      id: req.params.userId
-    }
-  }).then(result => {
-    res.status(200).json({
-      id: req.params.userId
-    }).catch(err => {
-      res.status(500).send('Error: ' + err);
-    })
-  })
-})
+router.get('/', controller.findAll);
+router.get('/:userId', controller.findByPk);
+router.post('/', controller.create)
+router.put('/:userId', controller.update)
+router.delete('/:userId', controller.destroy)
 
 module.exports = router;
