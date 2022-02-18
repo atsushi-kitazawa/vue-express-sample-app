cb405805620dafd3d18a22bfd55cca1e5a450d5e
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2022-02-19 00:14:51 +0900

    add database schema file.

---
+++ backend/db_schema/data.sql
@@ -0,0 +1,5 @@
+create database express_db;
+create table account (id integer primary key, name varchar(50));
+insert into account values (1, 'aaa');
+insert into account values (2, 'bbb');
+insert into account values (3, 'ccc');
