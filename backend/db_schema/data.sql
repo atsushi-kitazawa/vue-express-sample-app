31b9d094ebc5697f504e9abc4a7c5b7d6fec10f2
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2022-03-09 00:09:25 +0900

    add product table to data.sql

--- backend/db_schema/data.sql
+++ backend/db_schema/data.sql
@@ -3,3 +3,8 @@ create table account (id integer primary key, name varchar(50));
 insert into account values (1, 'aaa');
 insert into account values (2, 'bbb');
 insert into account values (3, 'ccc');
+
+create table product (id integer primary key, name varchar(50));
+insert into product values (1, 'aaa');
+insert into product values (2, 'bbb');
+insert into product values (3, 'ccc');
